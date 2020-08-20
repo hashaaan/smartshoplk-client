74fd96916de0b69034af1c082b1f8bd577d9373a
Hashan Shalitha <shalithavidanapathirana@gmail.com>
2020-08-20 13:23:41 +0530

    Initialize the project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="heading-1">Welcome to Smartshop.lk!</h1>
+    </div>
+  );
+}
+
+export default App;
