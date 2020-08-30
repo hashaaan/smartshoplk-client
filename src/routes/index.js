7bdc5685dd7cdf80dab17cfa3a4806f6ca6cbeb0
tmjayalath <tmjayalath.t@gmail.com>
2020-08-30 17:46:58 +0530

    Revert "New UIs"
    This reverts commit 07c22e0de5c315590f6b72b01276efc079ddd26b.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,28 +1,5 @@
 import Home from "../views/Home";
-import Smartphones from "../views/Smartphones";
-import ProductView from "../views/ProductView";
-import Cart from "../views/Cart";
 
-let indexRoutes = [
-  { path: "/", name: "Home", component: Home, exact: true },
-  {
-    path: "/smartphones",
-    name: "Smartphones",
-    component: Smartphones,
-    exact: true,
-  },
-  {
-    path: "/product",
-    name: "Smartphones",
-    component: ProductView,
-    exact: true,
-  },
-  {
-    path: "/cart",
-    name: "Shopping Cart",
-    component: Cart,
-    exact: true,
-  },
-];
+let indexRoutes = [{ path: "/", name: "Home", component: Home, exact: true }];
 
 export default indexRoutes;
