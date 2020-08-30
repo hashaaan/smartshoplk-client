519a604f3efceffe961b303e53d62b9726c1cf4d
Hashan Shalitha <shalithavidanapathirana@gmail.com>
2020-08-31 01:01:55 +0530

    Add dynamic cart items

--- src/views/Cart.jsx
+++ src/views/Cart.jsx
@@ -1,8 +1,38 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashAlt } from "@fortawesome/free-regular-svg-icons";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 import NavBar from "../components/layout/NavBar";
 //import Jumbotron from "./layout/Jumbotron";
 import "./Cart.css";
+
+let cartItems = [
+  {
+    name: "iPhone 11 Pro Max",
+    modelNo: "XFERRSA",
+    color: "Gold",
+    quantity: 3,
+    unitPrice: 12000,
+    currency: "LKR",
+  },
+  {
+    name: "iPhone 7 Plus",
+    modelNo: "XFERRSA",
+    color: "Red",
+    quantity: 5,
+    unitPrice: 120000,
+    currency: "LKR",
+  },
+  {
+    name: "OnePlus 8 Pro",
+    modelNo: "XFERRSA",
+    color: "Black",
+    quantity: 2,
+    unitPrice: 145000,
+    currency: "LKR",
+  },
+];
 
 class Cart extends Component {
   render() {
@@ -46,135 +76,57 @@ class Cart extends Component {
                         <th scope="col" width="120">
                           Quantity
                         </th>
-                        <th scope="col" width="120">
+                        <th scope="col" width="140">
                           Price
                         </th>
-                        <th scope="col" width="200" className="text-right">
+                        <th scope="col" width="180" className="text-right">
                           Action
                         </th>
                       </tr>
                     </thead>
                     <tbody>
-                      <tr>
-                        <td>
-                          <figure className="media">
-                            <div className="img-wrap">
-                              <img
-                                src="https://res.cloudinary.com/dxfq3iotg/image/upload/v1560924153/alcatel-smartphones-einsteiger-mittelklasse-neu-3m.jpg"
-                                className="img-thumbnail img-sm"
-                                alt="..."
-                              />
+                      {cartItems.map((item, index) => (
+                        <tr key={index}>
+                          <td>
+                            <figure className="media">
+                              <div className="img-wrap">
+                                <img
+                                  src="https://res.cloudinary.com/dxfq3iotg/image/upload/v1560924153/alcatel-smartphones-einsteiger-mittelklasse-neu-3m.jpg"
+                                  className="img-thumbnail img-sm"
+                                  alt="..."
+                                />
+                              </div>
+                              <figcaption className="media-body">
+                                <h6 className="title text-truncate">
+                                  {item.name}
+                                </h6>
+                                <dl className="param param-inline small">
+                                  <dt>Model No: </dt>
+                                  <dd>{item.modelNo}</dd>
+                                </dl>
+                                <dl className="param param-inline small">
+                                  <dt>Color: </dt>
+                                  <dd>{item.color}</dd>
+                                </dl>
+                              </figcaption>
+                            </figure>
+                          </td>
+                          <td>{item.quantity}</td>
+                          <td>
+                            <div className="price-wrap">
+                              <var className="price">Rs.12000</var>
+                              <small className="text-muted">
+                                ({item.currency} {item.unitPrice} each)
+                              </small>
                             </div>
-                            <figcaption className="media-body">
-                              <h6 className="title text-truncate">
-                                iPhone 11 Pro Max
-                              </h6>
-                              <dl className="param param-inline small">
-                                <dt>Model No: </dt>
-                                <dd>XFERRSA</dd>
-                              </dl>
-                              <dl className="param param-inline small">
-                                <dt>Color: </dt>
-                                <dd>Gold</dd>
-                              </dl>
-                            </figcaption>
-                          </figure>
-                        </td>
-                        <td>4</td>
-                        <td>
-                          <div className="price-wrap">
-                            <var className="price">Rs.12000</var>
-                            <small className="text-muted">
-                              (Rs.12000 each)
-                            </small>
-                          </div>
-                        </td>
-                        <td className="text-right">
-                          <button className="btn btn-outline-danger">
-                            Remove
-                          </button>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>
-                          <figure className="media">
-                            <div className="img-wrap">
-                              <img
-                                src="https://res.cloudinary.com/dxfq3iotg/image/upload/v1560924153/alcatel-smartphones-einsteiger-mittelklasse-neu-3m.jpg"
-                                className="img-thumbnail img-sm"
-                                alt="..."
-                              />
-                            </div>
-                            <figcaption className="media-body">
-                              <h6 className="title text-truncate">
-                                Product name goes here{" "}
-                              </h6>
-                              <dl className="param param-inline small">
-                                <dt>Model No: </dt>
-                                <dd>RRRSXXL</dd>
-                              </dl>
-                              <dl className="param param-inline small">
-                                <dt>Color: </dt>
-                                <dd>Red</dd>
-                              </dl>
-                            </figcaption>
-                          </figure>
-                        </td>
-                        <td>8</td>
-                        <td>
-                          <div className="price-wrap">
-                            <var className="price">Rs.12000</var>
-                            <small className="text-muted">
-                              (Rs.12000 each)
-                            </small>
-                          </div>
-                        </td>
-                        <td className="text-right">
-                          <button className="btn btn-outline-danger btn-round">
-                            Remove
-                          </button>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>
-                          <figure className="media">
-                            <div className="img-wrap">
-                              <img
-                                src="https://res.cloudinary.com/dxfq3iotg/image/upload/v1560924153/alcatel-smartphones-einsteiger-mittelklasse-neu-3m.jpg"
-                                className="img-thumbnail img-sm"
-                                alt="..."
-                              />
-                            </div>
-                            <figcaption className="media-body">
-                              <h6 className="title text-truncate">
-                                Product name goes here{" "}
-                              </h6>
-                              <dl className="param param-inline small">
-                                <dt>Model No: </dt>
-                                <dd>XXLDES</dd>
-                              </dl>
-                              <dl className="param param-inline small">
-                                <dt>Color: </dt>
-                                <dd>Black</dd>
-                              </dl>
-                            </figcaption>
-                          </figure>
-                        </td>
-                        <td>1</td>
-                        <td>
-                          <div className="price-wrap">
-                            <var className="price">Rs.12000</var>
-                            <small className="text-muted">
-                              (Rs.12000 each)
-                            </small>
-                          </div>
-                        </td>
-                        <td className="text-right">
-                          <button className="btn btn-outline-danger btn-round">
-                            Remove
-                          </button>
-                        </td>
-                      </tr>
+                          </td>
+                          <td className="text-right">
+                            <button className="btn btn-outline-danger">
+                              <FontAwesomeIcon icon={faTrashAlt} />
+                            </button>
+                          </td>
+                        </tr>
+                      ))}
                     </tbody>
                   </table>
 
@@ -190,7 +142,8 @@ class Cart extends Component {
                       className="button cart_button_clear"
                       onClick={() => history.push("/smartphones")}
                     >
-                      Continue Shopping
+                      <FontAwesomeIcon icon={faShoppingCart} /> Continue
+                      Shopping
                     </button>
                     <button
                       type="button"
