7fec9a85a196c0b4408f6667d68c7b6c8b0744cf
Hashan Shalitha <shalithavidanapathirana@gmail.com>
2020-09-09 18:46:40 +0530

    Remove unnecessary codes

--- src/models/member.js
+++ src/models/member.js
@@ -93,26 +93,6 @@ export default {
     },
 
     /**
-     * Listen for realtime updates on the current user
-     */
-    listenForMemberProfileUpdates() {
-      //   const UID =
-      //     FirebaseRef &&
-      //     Firebase &&
-      //     Firebase.auth() &&
-      //     Firebase.auth().currentUser &&
-      //     Firebase.auth().currentUser.uid
-      //       ? Firebase.auth().currentUser.uid
-      //       : null;
-      //   if (!UID) return false;
-      //   const ref = FirebaseRef.child(`users/${UID}`);
-      //   return ref.on("value", (snapshot) => {
-      //     const userData = snapshot.val() || [];
-      //     this.setUserDetails(userData); // Send to reducer
-      //   });
-    },
-
-    /**
      * Get the current Member's Details
      *
      * @returns {Promise}
@@ -216,90 +196,6 @@ export default {
             reject(err);
           });
       });
-    },
-
-    /**
-     * Reset Password
-     *
-     * @param {obj} formData - data from form
-     * @return {Promise}
-     */
-    resetPassword(formData) {
-      //   const { email } = formData;
-      //   return new Promise(async (resolve, reject) => {
-      //     // Validation rules
-      //     if (!email) return reject({ message: errorMessages.missingEmail });
-      //     // Go to Firebase
-      //     return Firebase.auth()
-      //       .sendPasswordResetEmail(email)
-      //       .then(() => {
-      //         this.resetUser();
-      //         resolve();
-      //       })
-      //       .catch(reject);
-      //   }).catch((err) => {
-      //     throw err.message;
-      //   });
-    },
-
-    /**
-     * Update Profile
-     *
-     * @param {obj} formData - data from form
-     * @return {Promise}
-     */
-    updateProfile(formData) {
-      //   const {
-      //     email,
-      //     password,
-      //     password2,
-      //     firstName,
-      //     lastName,
-      //     changeEmail,
-      //     changePassword,
-      //   } = formData;
-      //   return new Promise(async (resolve, reject) => {
-      //     // Are they a user?
-      //     const UID = await Firebase.auth().currentUser.uid;
-      //     if (!UID) return reject({ message: errorMessages.memberNotAuthd });
-      //     // Validation rules
-      //     if (!firstName)
-      //       return reject({ message: errorMessages.missingFirstName });
-      //     if (!lastName)
-      //       return reject({ message: errorMessages.missingLastName });
-      //     if (changeEmail) {
-      //       if (!email) return reject({ message: errorMessages.missingEmail });
-      //     }
-      //     if (changePassword) {
-      //       if (!password)
-      //         return reject({ message: errorMessages.missingPassword });
-      //       if (!password2)
-      //         return reject({ message: errorMessages.missingPassword });
-      //       if (password !== password2)
-      //         return reject({ message: errorMessages.passwordsDontMatch });
-      //     }
-      //     // Go to Firebase
-      //     return FirebaseRef.child(`users/${UID}`)
-      //       .update({ firstName, lastName })
-      //       .then(async () => {
-      //         // Update Email address
-      //         if (changeEmail) {
-      //           await Firebase.auth()
-      //             .currentUser.updateEmail(email)
-      //             .catch(reject);
-      //         }
-      //         // Change the Password
-      //         if (changePassword) {
-      //           await Firebase.auth()
-      //             .currentUser.updatePassword(password)
-      //             .catch(reject);
-      //         }
-      //         return resolve();
-      //       })
-      //       .catch(reject);
-      //   }).catch((err) => {
-      //     throw err.message;
-      //   });
     },
 
     /**
